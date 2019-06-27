47e05d810f4e4199f6d587a378173fe7b7282a2f
Stefan Indaco <indacostefan@gmail.com>
2019-06-27 11:15:56 +0200

    made custom swipe layout

--- jitpack/src/main/java/com/stefan/testjitpackrepo/StefanActivity.java
+++ jitpack/src/main/java/com/stefan/testjitpackrepo/StefanActivity.java
@@ -3,11 +3,12 @@ package com.stefan.testjitpackrepo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class StefanActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
 	}
 }
